d5bf6c7b4e5189c77dc2141486989e6a4e1e9459
Takido-NFT <malbecktao@gmail.com>
2025-04-03 11:12:49 +0200

    CPP00 - ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taomalbe <taomalbe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 10:11:15 by taomalbe          #+#    #+#             */
+/*   Updated: 2025/04/03 11:05:01 by taomalbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <locale>
+
+int main(int ac, char *av[])
+{
+	int	i;
+	int	j;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		i = 0;
+		while (av[++i])
+		{
+			j = 0;
+			while (av[i][j])
+				std::cout << (unsigned char)std::toupper(av[i][j++]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
