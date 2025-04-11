3c18d0d639da034e481319ddc3bbce6a865a228f
Taki-do <malbecktao@gmail.com>
2025-04-11 14:43:03 +0200

    bonus CPP01

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -5,13 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: taomalbe <taomalbe@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/11 10:46:40 by taomalbe          #+#    #+#             */
-/*   Updated: 2025/04/11 14:03:28 by taomalbe         ###   ########.fr       */
+/*   Created: 2025/04/11 14:04:30 by taomalbe          #+#    #+#             */
+/*   Updated: 2025/04/11 14:39:37 by taomalbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
-
 
 void Harl::debug( void ) {
 	std::cout << "DEBUG" << std::endl;
@@ -30,17 +29,46 @@ void Harl::error( void ) {
 }
 
 void Harl::complain( std::string level ) {
-	Harl 	harl;
+	int		index = -1;
 	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void (Harl::*message[4])(void) = {
-		&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 	
 	for (int i = 0; i < 4; i++)
 	{
 		if (levels[i] == level)
 		{
-			(harl.*message[i])();
+			index = i;
 			break;
 		}
+	}
+	switch (index)
+	{
+		case (0) :
+		{
+			debug();
+			info();
+			warning();
+			error();
+			break;
+		}
+		case (1) :
+		{
+			info();
+			warning();
+			error();
+			break;	
+		}
+		case (2) :
+		{
+			warning();
+			error();
+			break;	
+		}
+		case (3) :
+		{
+			error();
+			break;	
+		}
+		default :
+			std::cout << "Just stop harl.." << std::endl;
 	}
 }
--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -1,29 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taomalbe <taomalbe@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/11 10:44:28 by taomalbe          #+#    #+#             */
-/*   Updated: 2025/04/11 14:03:49 by taomalbe         ###   ########.fr       */
+/*   Created: 2025/04/11 14:04:12 by taomalbe          #+#    #+#             */
+/*   Updated: 2025/04/11 14:04:14 by taomalbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HARL_HPP
-#define HARL_HPP
+#include "Harl.hpp"
 
-#include <iostream>
-#include <string>
-
-class Harl {
-	private:
-		void debug( void );
-		void info( void );
-		void warning( void );
-		void error( void );
-	public:
-		void complain( std::string level );
-};
-
-#endif
+int	main(int ac, char *av[]) {
+	Harl harl;
+	if (ac == 2)
+	{
+		std::string str = std::string(av[1]);
+		harl.complain(str);
+	}
+}
