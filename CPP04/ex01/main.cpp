6e6ebaf632ecd6d908ab3787def934aadab08c50
Taki-do <malbecktao@gmail.com>
2025-05-05 16:48:16 +0200

    Nice main ex01 CPP04

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -6,18 +6,30 @@
 /*   By: taomalbe <taomalbe@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 11:34:39 by taomalbe          #+#    #+#             */
-/*   Updated: 2025/05/05 14:58:33 by taomalbe         ###   ########.fr       */
+/*   Updated: 2025/05/05 16:47:54 by taomalbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongAnimal.hpp"
 
-int main()
+int main(void)
 {
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	delete j;//should not create a leak
-	delete i;
-
+	int	size = 4;
+	
+	int	i = 0;
+	const Animal *tab[size];
+	
+	std::cout << "[Tab init started]" << std::endl << std::endl;
+	while (i < size / 2)
+		tab[i++] = new Dog();
+	std::cout << std::endl << "[Dog init finished]" << std::endl << std::endl;
+	while (i < size)
+		tab[i++] = new Cat();
+	std::cout << std::endl << "[Cat init finished]" << std::endl;
+	std::cout << std::endl << "[Tab init finished]" << std::endl << std::endl;
+	i = 0;
+	while (i < size)
+		delete tab[i++];
+	std::cout << std::endl << "[Tab delete finished]" << std::endl;
 	return 0;
 }
