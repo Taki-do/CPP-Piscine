ea7aeb30aa50de5888c26b5a0b853612dab05881
Takido-NFT <malbecktao@gmail.com>
2025-05-17 09:23:51 +0200

    PUBLIC

--- CPP03/ex02/FragTrap.hpp
+++ CPP03/ex02/FragTrap.hpp
@@ -6,14 +6,14 @@
 /*   By: taomalbe <taomalbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 12:56:09 by taomalbe          #+#    #+#             */
-/*   Updated: 2025/04/30 14:32:30 by taomalbe         ###   ########.fr       */
+/*   Updated: 2025/05/17 09:17:18 by taomalbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
 class FragTrap : public ClapTrap {
-    protected:
+    public:
         FragTrap();
         FragTrap(std::string str);
         FragTrap(const FragTrap& copy);
