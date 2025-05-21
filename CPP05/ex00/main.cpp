60b24671b886ba275079025610f9fae07cc12ced
Takido-NFT <malbecktao@gmail.com>
2025-05-21 14:15:23 +0200

    Start ex00 CPP05

---
+++ CPP05/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taomalbe <taomalbe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 11:55:55 by taomalbe          #+#    #+#             */
+/*   Updated: 2025/05/21 11:55:55 by taomalbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main() {
+	try {
+		Bureaucrat b(150, "bob");
+        std::cout << b;
+	} catch (const Bureaucrat::GradeTooHighException& e) {
+		std::cout << "Caught: " << e.what() << std::endl;
+	}
+    catch (const Bureaucrat::GradeTooLowException& e) {
+		std::cout << "Caught: " << e.what() << std::endl;
+	}
+}
